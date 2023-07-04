c272b2efe62c6a88317ada44f547b74ad6425ff2
glory <glory@gmail.com>
2023-07-04 21:56:00 +0900

    static 폴더 추가

---
+++ src/main/resources/static/js/check.js
@@ -0,0 +1,97 @@
+// JS 유효성 검사
+
+// 유효성 검사가 적용이 안되는 경우 : 대부분 id나 name의 오타일 가능성이 높고, 그게 아닐 경우에는 인터넷 어플리케이션을 바꿔가며 적용해볼것(ex chrome, microsoftEdge ...)
+
+// 회원가입 유효성 검사
+function signupcheck() {
+	
+	var memberId = document.getElementById('member_id');
+	var memberPassword = document.getElementById('memberPassword');
+	var contact = document.getElementById('memberContact');
+	var memberEmail = document.getElementById('memberEmail');
+	/*mailAuthCheck : 인증번호 일치 여부 확인*/
+	var mailAuthCheck = document.getElementById('email_check_boolean').checked;
+	/*mailInputCheck : 인증번호 입력 여부 확인*/
+	var mailInputCheck = document.getElementById('email_chk_boolean').checked;
+	var mailDuplicateCheck = document.getElementById('email_dup_boolean').checked;
+	var idDuplicateCheck = document.getElementById('id_check_boolean').checked;
+	var idCheck = document.getElementById('id_boolean').checked;
+	
+
+	
+	if(!mailInputCheck){
+		alert('인증번호를 입력하셔야합니다.');
+		return;
+	}
+	
+	if(!mailAuthCheck){
+		alert('인증번호가 일치 하지 않습니다. 메일 인증 절차를 다시 진행해주세요.');
+		return;
+	}
+	
+	if(!idCheck){
+		alert('아이디 중복확인를 진행해주세요.');
+		return;
+	}
+	
+	if(!idDuplicateCheck){
+		alert('중복된 아이디가 존재합니다. 아이디를 확인해주세요.');
+		return;
+	}
+	
+	if(!mailDuplicateCheck){
+		alert('중복된 이메일이 존재합니다. 이메일을 확인해주세요.');
+		return;
+	}
+
+	document.getElementById('memberForm').submit();
+
+
+}
+
+
+// 로그인 공백 확인
+function loginCheck(){
+	memberId = document.getElementById('floatingInput');
+	memberPassword = document.getElementById('floatingPassword');
+	console.log(memberId);
+	console.log(memberPassword);
+
+	if (memberId.value == "") {
+		alert('아이디를 입력해주세요.');
+		floatingInput.focus();
+		return;
+	} else if (memberPassword.value == "") {
+		alert('비밀번호를 입력해주세요.');
+		floatingPassword.focus();
+		return;
+		}
+		
+	document.getElementById('loginForm').submit();
+}
+
+/*회원 삭제 버튼*/
+
+function button_event(){
+		var inputId = document.getElementById('deleteId');
+		var inputPw = document.getElementById('deletePw');
+		
+		if(inputId.value == ""){
+			alert('아이디를 입력해주세요.');
+			return;
+		}else if(inputPw.value == ""){
+			alert('비밀번호를 입력해주세요.');
+			return;
+		}
+		
+		  if (confirm("정말 삭제하시겠습니까??") == true){    //확인
+			  document.getElementById('memberDeleteForm').submit();
+		  }else{   //취소
+		      return;
+		  }
+	}
+
+/* 회원 비밀번호 찾기 버튼 이벤트 */
+
+
+
---
+++ src/main/resources/static/js/menu/latteCount.js
@@ -0,0 +1,157 @@
+function countLatteList0(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount0');
+	  const resultCartAmount = document.getElementById('latteCartAmount0');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount0');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
+function countLatteList1(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount1');
+	  const resultCartAmount = document.getElementById('latteCartAmount1');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount1');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
+function countLatteList2(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount2');
+	  const resultCartAmount = document.getElementById('latteCartAmount2');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount2');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
+function countLatteList3(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount3');
+	  const resultCartAmount = document.getElementById('latteCartAmount3');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount3');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
+	
+function countLatteList4(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount4');
+	  const resultCartAmount = document.getElementById('latteCartAmount4');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount4');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
+	
+function countLatteList5(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount5');
+	  const resultCartAmount = document.getElementById('latteCartAmount5');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount5');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
+	
+function countLatteList6(type)  {
+	  // 결과를 표시할 element
+	  const resultCount = document.getElementById('latteCount6');
+	  const resultCartAmount = document.getElementById('latteCartAmount6');
+	  const resultOrderAmount = document.getElementById('latteOrderAmount6');
+	  
+	  // 현재 화면에 표시된 값
+	  let number = resultCount.innerText;
+	  
+	  // 더하기/빼기
+	  if(type === 'plus') {
+	    number = parseInt(number) + 1;
+	  }else if(type === 'minus' && number > 0)  {
+	    number = parseInt(number) - 1;
+	  }
+	  
+	  // 결과 출력
+	  resultCount.innerText = number;
+	  resultCartAmount.value = resultCount.innerText
+	  resultOrderAmount.value = resultCount.innerText
+	}
+	
