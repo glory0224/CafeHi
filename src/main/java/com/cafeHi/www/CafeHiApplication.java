5e88c77d3af1ffc26ac20b2a23a2d16f3d20a7b3
glory <glory@gmail.com>
2023-07-04 21:52:30 +0900

    MyBatis 관련 Mapper 삭제

--- src/main/java/com/cafeHi/www/CafeHiApplication.java
+++ src/main/java/com/cafeHi/www/CafeHiApplication.java
@@ -1,10 +1,8 @@
 package com.cafeHi.www;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(value = {"com.cafeHi.www.mapper"})
 @SpringBootApplication
 public class CafeHiApplication {
 
