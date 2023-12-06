7c6608f862ab2eefb5618563c166b3566b37893f
glory <glory@gmail.com>
2023-12-06 21:08:06 +0900

    SecurityConfig 내부 custom 방식 accessDeniedHandler 추가

--- src/main/java/com/cafeHi/www/common/security/config/SecurityConfig.java
+++ src/main/java/com/cafeHi/www/common/security/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.cafeHi.www.common.security.config;
 
 
+import com.cafeHi.www.common.security.common.FormAuthenticationDetailSource;
+import com.cafeHi.www.common.security.filter.AjaxLoginProcessingFilter;
+import com.cafeHi.www.common.security.handler.CustomAccessDeniedHandler;
 import com.cafeHi.www.common.security.provider.CustomAuthenticationProvider;
 import com.cafeHi.www.member.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,16 +19,19 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private final CustomUserDetailService customUserDetailService;
-	private final AuthenticationDetailsSource customAuthenticationDetailsSource;
+	private final FormAuthenticationDetailSource customAuthenticationDetailsSource;
 	private final AuthenticationSuccessHandler customAuthenticationSuccessHandler;
 	private final AuthenticationFailureHandler customAuthenticationFailureHandler;
 	
@@ -31,12 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// 인증을 무시하기 위한 설정
 		web.ignoring().antMatchers("/cafeHi/**","/js/**", "/css/**");
 	}
-	
-	
+
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.csrf().disable()	// Post Mapping 할 때 시큐리티는 csrf를 체크하면서 값이 없으면 403 에러를 발생시키는데 그 기능을 꺼주는 설정
+//                .csrf().disable()	// Post Mapping 할 때 시큐리티는 csrf를 체크하면서 값이 없으면 403 에러를 발생시키는데 그 기능을 꺼주는 설정
                 .authorizeRequests()
                 .antMatchers("/", "/CafeHi-Introduce", "/CafeHi-Membership",
 						"/CafeHi-Event", "/CafeHi-Place", "/CafeHi-Menu", "common/login*").permitAll()	// "common/login*" login 뒤에 어떤 문자열이 찍히더라도 경로로 인식하고 permitAll 처리한다.
@@ -44,8 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/CafeHi-MemberInfoUpdate").hasRole("USER")
                 .antMatchers("/CafeHi-MemberChangePassword").hasRole("USER")
                 .antMatchers("/CafeHi-MemberInfoDelete").hasRole("USER")
-                .antMatchers("/CafeHi-QnAWrite").hasRole("USER")                
-                .antMatchers("/CafeHi-UpdateQnA").hasRole("USER")                
+                .antMatchers("/CafeHi-QnAWrite").hasRole("USER")
+                .antMatchers("/CafeHi-UpdateQnA").hasRole("USER")
                 .antMatchers("/CafeHi-MemberMyPage").hasRole("USER")
 				.antMatchers("/CafeHi-MyMembership").hasRole("USER")
 				.antMatchers("/CafeHi-MyPageQnA").hasRole("USER")
@@ -55,24 +62,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/CafehiOrder").hasRole("USER")
                 .antMatchers("/CafeHi-MyPageOrderMenuList").hasRole("USER")
 
-                .and()
+		.and()
                 	.formLogin()
                     .loginPage("/login")	// 커스텀 login 페이지 이동
 					.successHandler(customAuthenticationSuccessHandler)
 					.failureHandler(customAuthenticationFailureHandler)
 					.authenticationDetailsSource(customAuthenticationDetailsSource)
                     .permitAll()
-                    .and()
-					;
+		.and()
+					.exceptionHandling()
+					.accessDeniedHandler(accessDeniedHandler());
     }
-	
-	
-	
-	
-    @Override
+
+	@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
     }
+
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		CustomAccessDeniedHandler accessDeniedHandler = new CustomAccessDeniedHandler();
+		accessDeniedHandler.setErrorPage("/denied");
+		return accessDeniedHandler;
+	}
+
+
+
 
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
